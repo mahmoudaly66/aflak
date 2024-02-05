71212366aa8aa826c2d905a35d3a20acdd843c2e
mahmoudaly66 <mahmoud666aly@gmail.com>
2024-02-05 17:27:43 +0200

    index

---
+++ js/main.js
@@ -0,0 +1,9 @@
+document.querySelectorAll(".nav-link").forEach((link) => {
+  if (link.href === window.location.href) {
+      link.classList.add("active");
+      link.setAttribute("aria-current", "page");
+  }
+});
+
+
+
